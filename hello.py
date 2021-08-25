20b0fe5c60c3cd6fa71983759df8762a86c37776
CE-Dev <emiliantivlica@yahoo.com>
2021-08-25 13:54:28 +0300

    Form Sumited and alert the user

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask.templating import render_template
 from flask_wtf import FlaskForm
 from flask_wtf.recaptcha import validators
@@ -64,6 +64,7 @@ def name():
  if form.validate_on_submit():
      name = form.name.data
      form.name.data =  ''
+     flash("Form Submited Succesfully")
 
  return render_template("name.html",
      name = name,
